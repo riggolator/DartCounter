1cc98639fdddc554ce172008ee6d261505399961
Rico Wenzel <rico.wenzel@fabmatics.com>
2025-02-01 02:22:11 +0100

    wip

--- include/StatisticWindow.h
+++ include/StatisticWindow.h
@@ -2,6 +2,7 @@
 #define STATISTIC_WINDOW_H
 
 #include "../include/Player.h"
+#include <map>
 #include <string>
 #include <vector>
 #include <wx/listbox.h>
@@ -9,10 +10,39 @@
 
 class StatisticWindow : public wxFrame {
 public:
-  StatisticWindow(std::vector<Player> &);
+  StatisticWindow(std::vector<Player> const &);
   void OnClose(wxCommandEvent &event);
+  void OnPlayerSelection(wxCommandEvent &event);
 
 private:
+  std::vector<Player> players;
+  wxChoice *playerDropdown;
+  wxStaticText *winRateLabel;
+  wxPanel *chartPanel;
+  wxPanel *heatmapPanel;
+  void UpdateStatistics(int playerIndex);
+  void DrawAverageChart(const Player &player);
+  void DrawHeatmap(const Player &player);
 };
 
 #endif // STATISTIC_WINDOW_H
+
+// #ifndef STATISTIC_WINDOW_H
+// #define STATISTIC_WINDOW_H
+
+// #include "../include/Player.h"
+// #include <string>
+// #include <vector>
+// #include <wx/listbox.h>
+// #include <wx/wx.h>
+
+// class StatisticWindow : public wxFrame {
+// public:
+// std::vector<Player> players;
+// StatisticWindow(std::vector<Player> &);
+// void OnClose(wxCommandEvent &event);
+
+// private:
+// };
+
+// #endif // STATISTIC_WINDOW_H
